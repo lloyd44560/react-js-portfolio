3926218179924a5094ed49b577030682eb14cc54
Your Name <jhonlloydborlagdan8@gmail.com>
2024-06-04 21:11:31 +0800

    1 commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Simple React Page</h1>
+        <p>This is a simple page created with ReactJS.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
